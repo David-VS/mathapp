5f3b1a9567f907e96118b217fa5db03b5eb3c6db
david.van.steertegem@gmail.com <Langwachtwoord1>
2020-05-12 15:12:30 +0200

    added methods for abs value and sums added testcases

--- src/main/java/be/ehb/mathapp/utils/Calculator.java
+++ src/main/java/be/ehb/mathapp/utils/Calculator.java
@@ -1,4 +1,12 @@
 package be.ehb.mathapp.utils;
 
 public class Calculator {
+
+    public static int sum(int a, int b){
+        return b + a;
+    }
+
+    public static int absolute(int a){
+        return Math.abs(a);
+    }
 }
---
+++ src/test/java/be/ehb/mathapp/CalculatorTest.java
@@ -0,0 +1,29 @@
+package be.ehb.mathapp;
+
+import be.ehb.mathapp.utils.Calculator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class CalculatorTest {
+
+    @Test
+    public void shouldAdd(){
+        assertEquals(25, Calculator.sum(15,10));
+    }
+
+    @Test
+    public void shouldAddNegative(){
+        assertEquals(5, Calculator.sum(7, -2));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {42, -8, 0, 8547853})
+    public void shouldBeAbsolute(int param){
+        assertTrue(Calculator.absolute(param) >= 0);
+    }
+}
